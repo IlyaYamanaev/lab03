1f54e6a3ae8ffae6d3f8b23b2cae27b9f71be16e
IlyaYamanaev <166473851+IlyaYamanaev@users.noreply.github.com>
2024-04-13 20:13:58 +0300

    add files

---
+++ Client.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+
+// буфер для входящих данных
+
+bool clientStopped = false;
+
+while (!clientStopped)
+{
+    using TcpClient client = new TcpClient();
+    await client.ConnectAsync("127.0.0.1", 8888);
+    var stream = client.GetStream();
+    Console.Write("\nEnter action (1 - get a file, 2 - send a file, 3 - delete a file, 4 - show all file): > ");
+    var action = Console.ReadLine();
+    while (action != "1" && action != "2" && action != "3" && action != "4" && action != "exit")
+    {
+        Console.Write("Wrong command. Repeat: > ");
+        action = Console.ReadLine();
+    }
+
+    var file_IDorNAME = "";
+    var filenameInClientData = "";
+    var newFilenameForServer = "";
+    var byIDorNAME = 0;
+
+
+
+    if (action == "1" || action == "3")
+    {
+        Console.Write("Do you want to get the file by ID(1) or by name(2): > ");
+        byIDorNAME = int.Parse(Console.ReadLine());  // 1 - ID; 2 - name
+        while (byIDorNAME != 1 && byIDorNAME != 2)
+        {
+            Console.Write("Input \"1\" or \"2\": > ");
+            byIDorNAME = int.Parse(Console.ReadLine());
+        }
+        string bufflineforconcole = "file ID";
+        if (byIDorNAME == 2) bufflineforconcole = "file name";
+        Console.Write($"Enter {bufflineforconcole}: > ");
+        file_IDorNAME = Console.ReadLine();
+    }
+
+    if (action == "2")
+    {
+        Console.Write("Enter name of the file: > ");
+        filenameInClientData = "C:\\Users\\depor\\Desktop\\" + Console.ReadLine();
+
+        Console.Write("Enter name of the file to be saved on server: > ");
+        newFilenameForServer = Console.ReadLine();
+        if (newFilenameForServer == "") newFilenameForServer = filenameInClientData;
+    }
+
+
+    var requestToServer = "";
+    switch (action)
+    {
+        case "1":
+            requestToServer = $"GET {byIDorNAME} {file_IDorNAME}";
+            break;
+        case "2":
+            requestToServer = $"PUT {newFilenameForServer}";
+            break;
+        case "3":
+            requestToServer = $"DEL {file_IDorNAME} {newFilenameForServer}";
+            break;
+        case "4":
+            requestToServer = "SHOW ";
+            break;
+        case "exit":
+            requestToServer = "EXIT ";
+            break;
+        default:
+            break;
+    }
+
+    byte[] byteRequest = Encoding.UTF8.GetBytes(requestToServer);
+    await stream.WriteAsync(byteRequest, 0, byteRequest.Length);
+    Console.WriteLine("The request was sent.");
+
+    if (action == "exit")
+    {
+        Console.WriteLine("Serever stopped");
+        break;
+    }
+
+    byte[] byterespFromServer = new byte[254];
+    int bytes = stream.Read(byterespFromServer, 0, byterespFromServer.Length);
+    var arrRespFromServer = Encoding.UTF8.GetString(byterespFromServer, 0, bytes).Split('~');
+    var codeResp = int.Parse(arrRespFromServer[0]);
+
+    switch (codeResp)
+    {
+        case 201:
+            byte[] fileSizeBytes = new byte[4];
+            stream.Read(fileSizeBytes, 0, fileSizeBytes.Length);
+            int fileSize = BitConverter.ToInt32(fileSizeBytes, 0);
+
+            byte[] imageData = new byte[fileSize];
+            int bytesRead = stream.Read(imageData, 0, fileSize);
+            Console.Write("The file was downloaded! Specify a name for it: > ");
+            string filePath = "C:\\Users\\depor\\Desktop\\" + Console.ReadLine();
+            File.WriteAllBytes(filePath, imageData);
+            Console.WriteLine("File saved on the hard drive!");
+            break;
+        case 401:
+            Console.WriteLine("The response says that this file is not found!");
+            break;
+        case 202:
+            Console.Write("Response says that file is saved!");
+
+            byte[] fileBytes = File.ReadAllBytes(filenameInClientData);
+            byte[] f1leSizeBytes = BitConverter.GetBytes(fileBytes.Length);
+            stream.Write(f1leSizeBytes, 0, f1leSizeBytes.Length);
+            stream.Write(fileBytes, 0, fileBytes.Length);
+
+            Console.WriteLine($"ID = {arrRespFromServer[1]}");
+
+            break;
+        case 402:
+            Console.Write("Response says that file is NOT saved!");
+            break;
+        case 203:
+            Console.Write("The response says that this file was deleted successfully!");
+            break;
+        case 403:
+            Console.WriteLine("The response says that this file is not found!");
+            break;
+        case 204:
+            Console.WriteLine(arrRespFromServer[1]);
+            break;
+        case 404:
+            Console.WriteLine("ERROR");
+            break;
+        default:
+            break;
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using System.CodeDom.Compiler;
+
+void SendStrToClient(string line,ref NetworkStream stream)
+{
+    byte[] byteResponseToCLient = Encoding.UTF8.GetBytes(line);
+    stream.WriteAsync(byteResponseToCLient, 0, byteResponseToCLient.Length);
+}
+
+var tcpListener = new TcpListener(IPAddress.Any, 8888);
+var serverStopped = false;
+Dictionary<int, string> serverDB = new Dictionary<int, string>();
+tcpListener.Start();    // запускаем сервер
+Console.WriteLine("The server is running.");
+var filedb = new StreamReader("C:\\Users\\depor\\Desktop\\proga\\cSharp\\FileServer_v2\\serverDB.txt");
+while (!filedb.EndOfStream)
+{
+    var line = filedb.ReadLine().Split(' ');
+    serverDB.Add(int.Parse(line[0]), line[1]);
+}
+filedb.Close();
+
+
+while (!serverStopped)
+{
+    //if (serverStopped == true) break;
+    var tcpClient = await tcpListener.AcceptTcpClientAsync();
+    Task.Run(async () => await ProcessClientAsync(tcpClient));
+}
+
+tcpListener.Stop();
+
+
+async Task ProcessClientAsync(TcpClient tcpClient)
+{
+    Console.WriteLine($"The client {tcpClient.Client.RemoteEndPoint} connected.");
+
+    var stream = tcpClient.GetStream();
+    byte[] byteRequestFromClient = new byte[256];
+    int bytes = stream.Read(byteRequestFromClient, 0, byteRequestFromClient.Length);
+    var requestFromClient = Encoding.UTF8.GetString(byteRequestFromClient, 0, bytes);
+    var arrForUnderstandingRequest = requestFromClient.Split(' ');
+    string command = arrForUnderstandingRequest[0];
+    string filename = "C:\\Users\\depor\\Desktop\\proga\\cSharp\\FileServer_v2\\serverData\\";
+
+    if (command == "EXIT")
+    {
+        var sw = new StreamWriter("C:\\Users\\depor\\Desktop\\proga\\cSharp\\FileServer_v2\\serverDB.txt");
+        foreach (var item in serverDB)
+        {
+            sw.WriteLine($"{item.Key} {item.Value}");
+        }
+        Console.WriteLine("dfslkghsferh;jwflrhuinecgwmnpgiprmojnehr;liefmj");
+        sw.Close(); 
+        serverStopped = true;  
+        return;
+    }
+    switch (command)
+    {
+        case "GET":
+            if (arrForUnderstandingRequest[1] == "1")
+            {
+                filename += serverDB[int.Parse(arrForUnderstandingRequest[2])];
+            }
+            else filename += arrForUnderstandingRequest[2];
+
+            if (File.Exists(filename))
+            {
+                SendStrToClient("201", ref stream);
+                byte[] fileBytes = File.ReadAllBytes(filename);
+                byte[] f1leSizeBytes = BitConverter.GetBytes(fileBytes.Length);
+                stream.Write(f1leSizeBytes, 0, f1leSizeBytes.Length);
+                stream.Write(fileBytes, 0, fileBytes.Length);
+            }
+            else SendStrToClient("401", ref stream);
+            break;
+
+        case "PUT":
+            filename += arrForUnderstandingRequest[1];
+            int rnd = 111;
+            while (serverDB.ContainsKey(rnd))
+            {
+                Random random = new Random();
+                rnd = random.Next(100, 1000);
+            }
+            serverDB.Add(rnd, arrForUnderstandingRequest[1]);
+            SendStrToClient($"202~{rnd}", ref stream);
+
+            byte[] fileSizeBytes = new byte[4];
+            stream.Read(fileSizeBytes, 0, fileSizeBytes.Length);
+            int fileSize = BitConverter.ToInt32(fileSizeBytes, 0);
+            byte[] imageData = new byte[fileSize];
+            int bytesRead = stream.Read(imageData, 0, fileSize);
+            File.WriteAllBytes(filename, imageData);
+
+            break;
+
+        case "DELETE":
+            if (arrForUnderstandingRequest[1] == "1")
+            {
+                filename += serverDB[int.Parse(arrForUnderstandingRequest[2])];
+            }
+            else filename += arrForUnderstandingRequest[2];
+            if (File.Exists(filename))
+            {
+                try
+                {
+                    File.Delete(filename);
+                    Console.WriteLine("file is delete\n");
+                    SendStrToClient("203", ref stream);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("ERROR 404\n");
+                    SendStrToClient("403", ref stream);
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR 403\n");
+                SendStrToClient("403", ref stream);
+            }
+            break;
+
+        case "SHOW":
+            var resp = "";
+            foreach (var item in serverDB)
+            {
+                resp += $"{item.Key}\t{item.Value}\n";
+            }
+            SendStrToClient($"204~{resp}", ref stream);
+            break;        
+        default:
+            break;
+    }
+    Console.WriteLine($"Response to client was send\n");
+    tcpClient.Close();
+}
